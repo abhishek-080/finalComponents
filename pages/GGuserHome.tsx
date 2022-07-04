72134989525dcd928d6fe53194fba54bf04b6ed9
Abhishek Kunwar <85834128+abhishek-080@users.noreply.github.com>
2022-07-04 21:38:00 +0545

    Added Home and new login module

---
+++ pages/GGuserHome.tsx
@@ -0,0 +1,39 @@
+import FollowSuggest from "../Components/Home/FollowSuggest"
+import Middlenav from "../Components/Home/Middlenav"
+import Posts from "../Components/Home/Posts"
+import Post_box from "../Components/Home/Post_box"
+
+const GGuserHome = () => {
+  return (
+    <div>
+        <div className="col-lg-5">
+            
+        <Post_box />
+        </div>
+        <Middlenav />
+            <div className="row">
+                <div className=" row posts col-lg-8">
+
+                <div className="col-lg-6 col-md-6">
+                  <Posts />
+                </div>
+                <div className="col-lg-6 col-md-6">
+                  <Posts />
+                </div>
+                <div className="col-lg-6 col-md-6">
+                  <Posts />
+                </div><div className="col-lg-6 col-md-6">
+                  <Posts />
+                </div>
+              
+                </div>
+      
+              <div className="col-lg-4">
+                  <FollowSuggest />
+                </div>
+              </div>
+        </div>
+  )
+}
+
+export default GGuserHome
